326cb380d3e16899c1453f222c03d75bb9c331da
savithande <38084956+savithande@users.noreply.github.com>
2019-11-05 17:50:09 +0530

    Add files via upload

---
+++ input_output.py
@@ -0,0 +1,15 @@
+
+#print commana is used to print directly
+print("hello")
+
+#input() is used to take the input from the user
+name = input()
+
+#assign the user input to the one variable and print that variable
+
+user_input = input()
+print(user_input)
+
+#numbers printing
+
+print("5" *5)
---
+++ python_variables.py
@@ -0,0 +1,41 @@
+#sytax to create the variable is
+#    'variable_name' = 'value of the variable name'
+
+name = "savitha"
+print(name)
+
+number1 = 10
+number2 = 20
+
+print(number1,number2)
+
+#assign the decimal number to the variable
+
+pi = 3.14
+print(pi)
+
+#assign the string value to the variable
+
+fav_language = "python"
+print (fav_language)
+print ("\n")
+
+#assign the function to the variable
+class my_class:
+    pass
+
+result = my_class()
+print (result)
+print("\n")
+
+#assigning list to the variable
+
+list1 = ["tiger","lion","elephant","chitha"]
+print(list1)
+print("\n")
+
+
+#assigning the dictinary to the variable
+
+dict = {"animal":"tiger","flower":"lotas","bird":"peacock","fruit":"mango"}
+print(dict)
